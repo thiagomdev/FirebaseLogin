4f8f93d2810432828131d1d04c60d782bd3f3e8c
Thiago Monteiro <thiago.monteiro.ext@sodexo.com>
2022-01-29 20:27:04 -0300

    User Login with Firebase - Programmatically

---
+++ UserLoginFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserLoginFirebase
+//
+//  Created by Thiago Monteiro on 29/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
